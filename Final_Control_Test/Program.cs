60447866c0184693dea6ecb37ba2ec03d1d98fe8
Enrik <Enrik.n1rvan@gmail.com>
2022-07-28 13:06:30 +0300

    add folder Final Control Test and solution code in Program.cs file

---
+++ Final_Control_Test/Program.cs
@@ -0,0 +1,34 @@
+﻿int SizeArray(string[] array) {
+    int n = 0;
+    for (int i = 0; i < array.Length; i++) {  
+        if (array[i].Length <= 3) {  
+            n++;
+        }
+    }
+    return n;
+}
+
+string[] CreateArray(string[] array, string[] newArray) {   
+    int index = 0;
+    for (int i = 0; i < array.Length; i++) {  
+        if (array[i].Length <= 3) {  
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+void ShowArray(string[] newArray) {
+    for (int i = 0; i < newArray.Length; i++) {
+        Console.Write($"{newArray[i]} ");
+    }
+}
+
+string[] array = {"-2", "Geek Brains","hello", ";-)", "13", "world", "cat"};  
+string[] newArray = new string[SizeArray(array)];  
+
+
+CreateArray(array, newArray);
+ShowArray(newArray);
+Console.WriteLine();
